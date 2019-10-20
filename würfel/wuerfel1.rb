46f3f79396be83a251a1a2f1450001649293535a
Sajonara <atrust@sajonara.de>
2019-10-21 00:44:43 +0200

    Script hinzufügen, das einen sechsseitigen Würfel simuliert.

---
+++ würfel/wuerfel1.rb
@@ -0,0 +1,9 @@
+# Das Programm simuliert einen Würfel.
+
+wuerfel = rand(6) + 1
+
+puts("Der Würfel zeigt #{wuerfel}.")
+
+# Diese Ausgabe zeigt an, welche Methoden das Objekt bereits alle besitzt.
+# Auf diese Weise kann man in Erfahrung bringen, wie man damit weiterarbeiten könnte.
+p wuerfel.methods
