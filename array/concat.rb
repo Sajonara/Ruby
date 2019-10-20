bd81bc5618cc1984b8281c0099d6892d9ddff543
Sajonara <atrust@sajonara.de>
2019-10-21 01:07:20 +0200

    Script hinzufügen, um Feld-Methode concat zu präsentieren.

---
+++ array/concat.rb
@@ -0,0 +1,8 @@
+# Das Programm dient als Beispiel, wie man Felder effizienter statt mit Addition zusammenführen kann
+
+# Zwei Testfeld instanziieren.
+a = [1,2,3]
+b = [4,5,6]
+
+p a.concat(b) # Feld b mit Feld a "zusammenfügen"
+p a # das neue Feld a ausgeben
